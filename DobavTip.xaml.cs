8a28c7085e22326bd5fcb16be5574b4b324da1e9
Your Name <you@example.com>
2023-03-24 21:39:14 +0300

    y_b

---
+++ DobavTip.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace budget_accounting
+{
+    /// <summary>
+    /// Логика взаимодействия для DobavTip.xaml
+    /// </summary>
+    public partial class DobavTip : Window
+    {
+        public string tip = "";
+        public DobavTip()
+        {
+            InitializeComponent();
+        }
+
+        private void DobavZap_Click(object sender, RoutedEventArgs e)
+        {
+            tip = Name.Text;
+            MessageBox.Show(tip);
+            MainWindow wind = new MainWindow();
+            wind.Okno(tip);
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace budget_accounting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static List<string> list = new List<string>();
+        private string item = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            Tips.ItemsSource = list;
+        }
+
+        private void Nov_tip_Click(object sender, RoutedEventArgs e)
+        {
+            DobavTip wind = new DobavTip();
+            wind.Show();
+        }
+
+        public void  Okno(string tip)
+        {
+            item = tip;
+            MessageBox.Show(item);
+            list.Add(item);
+            Zapolnenie();
+        }
+
+        private void Zapolnenie()
+        {
+            
+        }
+    }
+}
